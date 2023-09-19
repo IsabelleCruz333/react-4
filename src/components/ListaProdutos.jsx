5d5f90bd7d438cf0ad2baacb4630b528261e222a
IsabelleCruz333 <isabellecruz333@gmail.com>
2023-09-19 11:43:50 -0300

    react-4

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,6 @@
+export const ListaProdutos =[
+    {id: 1,nome: 'Teclado Gamer', desc: 'Mecanico', preço: 200},
+    {id: 1,nome: 'Mouse Gamer', desc: 'Mecanico', preço: 300},
+    {id: 1,nome: 'Processador Gamer', desc: 'Mecanico', preço: 400},
+    {id: 1,nome: 'Monitor Gamer', desc: 'Mecanico', preço: 200},
+]
